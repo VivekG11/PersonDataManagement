4d9d86146e5171ca1d4d63c5d228e5ea67621f50
VivekG11 <vivekvk2903@gmail.com>
2021-07-24 21:47:39 +0530

    [Displaying details of persons in list]

---
+++ LambdaExpressions/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpressions
+{
+    public class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+
+        public Person(string ssn, string name, string address, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Age = age;
+            Address = address;
+           
+        }
+
+        
+    }
+    
+}
---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaExpressions
+{
+    public class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<Person> people = new List<Person>();
+            AddDetails(people);
+            RetrieveAge( people);
+
+           
+        }
+
+        public static void AddDetails(List<Person> people)
+        {
+            people.Add(new Person("1234", "Vivek", "LB Nagar", 29));
+            people.Add(new Person("3421", "Aditya", "Kukatpally", 37));
+            people.Add(new Person("1129", "Vinay", "Chennai", 54));
+            people.Add(new Person("3901", "Pavani", "Kadapa", 35));
+            people.Add(new Person("6672", "Guna", "Vizag", 26));
+
+
+            foreach(Person i in people)
+            {
+              //  Console.WriteLine("Details are :"+i.SSN+","+i.Name+","+i.Address+","+i.Age);
+                Console.WriteLine("Details are {0} , {1} , {2}, {3} .",i.SSN,i.Name,i.Address,i.Age);
+            }
+
+          
+        }
+         public static void RetrieveAge(List<Person> people )
+        {
+            foreach (Person i in people.FindAll (a => (a.Age < 60)).Take(2).ToList()) 
+            {
+                Console.WriteLine("Name :"+i.Name + "Age :"+i.Age);
+            }
+            foreach (Person person in people.FindAll(b => (b.Age > 12 && b.Age < 50)).ToList())
+            {
+                Console.WriteLine("Name :" + person.Name + "Age :" + person.Age);
+            }
+        }
+        
+    }
+}
